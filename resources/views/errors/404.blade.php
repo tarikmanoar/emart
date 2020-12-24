06ed7a2f21c949f4701b82ee8f63cd9678953158
tarikmanoar <tarikmanoar@gmail.com>
2020-12-24 18:26:54 +0600

    Move assets

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,32 +5,32 @@
 		<meta charset="utf-8">
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-		<meta name="description" content="">
-		<meta name="author" content="">
-	    <meta name="keywords" content="MediaCenter, Template, eCommerce">
+		<meta name="description" content="tarikmanoar">
+		<meta name="author" content="tarikmanoar">
+	    <meta name="keywords" content="tarikmanoar, emarts, eCommerce">
 	    <meta name="robots" content="all">
 
-	    <title>Flipmart premium HTML5 & CSS3 Template</title>
+	    <title>{{ config('app.name', 'Laravel') }}</title>
 
 	    <!-- Bootstrap Core CSS -->
-	    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-	    
+	    <link rel="stylesheet" href="{{asset('frontend/assets/css/bootstrap.min.css')}}">
+
 	    <!-- Customizable CSS -->
-	    <link rel="stylesheet" href="assets/css/main.css">
-	    <link rel="stylesheet" href="assets/css/blue.css">
-	    <link rel="stylesheet" href="assets/css/owl.carousel.css">
-		<link rel="stylesheet" href="assets/css/owl.transitions.css">
-		<link rel="stylesheet" href="assets/css/animate.min.css">
-		<link rel="stylesheet" href="assets/css/rateit.css">
-		<link rel="stylesheet" href="assets/css/bootstrap-select.min.css">
+	    <link rel="stylesheet" href="{{asset('frontend/assets/css/main.css')}}">
+	    <link rel="stylesheet" href="{{asset('frontend/assets/css/blue.css')}}">
+	    <link rel="stylesheet" href="{{asset('frontend/assets/css/owl.carousel.css')}}">
+		<link rel="stylesheet" href="{{asset('frontend/assets/css/owl.transitions.css')}}">
+		<link rel="stylesheet" href="{{asset('frontend/assets/css/animate.min.css')}}">
+		<link rel="stylesheet" href="{{asset('frontend/assets/css/rateit.css')}}">
+		<link rel="stylesheet" href="{{asset('frontend/assets/css/bootstrap-select.min.css')}}">
 
-		
 
-		
+
+
 		<!-- Icons/Glyphs -->
-		<link rel="stylesheet" href="assets/css/font-awesome.css">
+		<link rel="stylesheet" href="{{asset('frontend/assets/css/font-awesome.css')}}">
 
-        <!-- Fonts --> 
+        <!-- Fonts -->
 		<link href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,700' rel='stylesheet' type='text/css'>
 		<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,600,600italic,700,700italic,800' rel='stylesheet' type='text/css'>
         <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
@@ -88,8 +88,8 @@
 					<!-- ============================================================= LOGO ============================================================= -->
 <div class="logo">
 	<a href="home.html">
-		
-		<img src="assets/images/logo.png" alt="">
+
+		<img src="{{asset('frontend/assets/images/logo.png" alt')}}="">
 
 	</a>
 </div><!-- /.logo -->
@@ -120,7 +120,7 @@
 
             <input class="search-field" placeholder="Search here..." />
 
-            <a class="search-button" href="#" ></a>    
+            <a class="search-button" href="#" ></a>
 
         </div>
     </form>
@@ -143,8 +143,8 @@
 						<span class="sign">$</span><span class="value">600.00</span>
 					</span>
 				</div>
-				
-			
+
+
 		    </div>
 		</a>
 		<ul class="dropdown-menu">
@@ -153,11 +153,11 @@
 					<div class="row">
 						<div class="col-xs-4">
 							<div class="image">
-								<a href="detail.html"><img src="assets/images/cart.jpg" alt=""></a>
+								<a href="detail.html"><img src="{{asset('frontend/assets/images/')}}cart.jpg" alt=""></a>
 							</div>
 						</div>
 						<div class="col-xs-7">
-							
+
 							<h3 class="name"><a href="index.php?page-detail">Simple Product</a></h3>
 							<div class="price">$600.00</div>
 						</div>
@@ -168,19 +168,19 @@
 				</div><!-- /.cart-item -->
 				<div class="clearfix"></div>
 			<hr>
-		
+
 			<div class="clearfix cart-total">
 				<div class="pull-right">
-					
+
 						<span class="text">Sub Total :</span><span class='price'>$600.00</span>
-						
+
 				</div>
 				<div class="clearfix"></div>
-					
-				<a href="checkout.html" class="btn btn-upper btn-primary btn-block m-t-20">Checkout</a>	
+
+				<a href="checkout.html" class="btn btn-upper btn-primary btn-block m-t-20">Checkout</a>
 			</div><!-- /.cart-total-->
-					
-				
+
+
 		</li>
 		</ul><!-- /.dropdown-menu-->
 	</div><!-- /.dropdown-cart -->
@@ -210,7 +210,7 @@
 		<ul class="nav navbar-nav">
 			<li class="active dropdown yamm-fw">
 				<a href="home.html" data-hover="dropdown" class="dropdown-toggle" data-toggle="dropdown">Home</a>
-				
+
 			</li>
 			<li class="dropdown yamm mega-menu">
 				<a href="home.html" data-hover="dropdown" class="dropdown-toggle" data-toggle="dropdown">Clothing</a>
@@ -218,7 +218,7 @@
 					<li>
                						<div class="yamm-content ">
             <div class="row">
-                
+
                    <div class="col-xs-12 col-sm-6 col-md-2 col-menu">
                         <h2 class="title">Men</h2>
                         <ul class="links">
@@ -229,7 +229,7 @@
                             <li><a href="#">Sport Wear</a></li>
                              <li><a href="#">Blazers</a></li>
                               <li><a href="#">Shirts</a></li>
-                          
+
                         </ul>
                     </div><!-- /.col -->
 
@@ -238,12 +238,12 @@
                         <ul class="links">
                             <li><a href="#">Handbags</a></li>
                             <li><a href="#">Jwellery</a></li>
-                            <li><a href="#">Swimwear </a></li>                   
+                            <li><a href="#">Swimwear </a></li>
                             <li><a href="#">Tops</a></li>
                             <li><a href="#">Flats</a></li>
                              <li><a href="#">Shoes</a></li>
                               <li><a href="#">Winter Wear</a></li>
-                       
+
                         </ul>
                     </div><!-- /.col -->
 
@@ -255,43 +255,43 @@
                             <li><a href="#">Shirts</a></li>
                             <li><a href="#">Shoes</a></li>
                              <li><a href="#">School Bags</a></li>
-                              <li><a href="#">Lunch Box</a></li> 
+                              <li><a href="#">Lunch Box</a></li>
                                <li><a href="#">Footwear</a></li>
-                                                                   
+
                         </ul>
                     </div><!-- /.col -->
 
                     <div class="col-xs-12 col-sm-6 col-md-2 col-menu">
                         <h2 class="title">Girls</h2>
                         <ul class="links">
-                            <li><a href="#">Sandals </a></li> 
+                            <li><a href="#">Sandals </a></li>
                             <li><a href="#">Shorts</a></li>
                             <li><a href="#">Dresses</a></li>
                             <li><a href="#">Jwellery</a></li>
                             <li><a href="#">Bags</a></li>
                              <li><a href="#">Night Dress</a></li>
                               <li><a href="#">Swim Wear</a></li>
-                          
-                                   
+
+
                         </ul>
                     </div><!-- /.col -->
 
-                    
+
        <div class="col-xs-12 col-sm-6 col-md-4 col-menu banner-image">
-                                    <img class="img-responsive" src="assets/images/banners/top-menu-banner.jpg" alt="">
-                              
-                            
-      
-   
-       
- 
-</div><!-- /.yamm-content -->					
+                                    <img class="img-responsive" src="{{asset('frontend/assets/')}}images/banners/top-menu-banner.jpg" alt="">
+
+
+
+
+
+
+</div><!-- /.yamm-content -->
 </div>
 </div>
 
 </li>
 				</ul>
-				
+
 			</li>
 
 			<li class="dropdown mega-menu">
@@ -347,7 +347,7 @@
                      <li><a href="#">Lenses</a></li>
                      <li><a href="#">Surveillance</a></li>
                       <li><a href="#">Tripods</a></li>
-                     
+
                 </ul>
             </div><!-- /.col -->
             <div class="col-xs-12 col-sm-12 col-md-2 col-menu">
@@ -365,16 +365,16 @@
                      <li><a href="#">Memory Cards</a></li>
                 </ul>
             </div>
-            
+
              <div class="col-xs-12 col-sm-12 col-md-4 col-menu custom-banner">
-             <a href="#"><img alt="" src="assets/images/banners/banner-side.png"></a>
+             < href="#"><img alt="" src="{{asset('frontend/assets/images/banners/banner-side.png"></')}}a>
              </div>
     </div><!-- /.row -->
 </div><!-- /.yamm-content -->					</li>
 				</ul>
 			</li>
 			<li class="dropdown hidden-sm">
-				
+
 				<a href="category.html">Health & Beauty
 				    <span class="menu-label new-menu hidden-xs">new</span>
 				</a>
@@ -387,21 +387,21 @@
 			<li class="dropdown">
 				<a href="contact.html">Jewellery</a>
 			</li>
-            
+
             <li class="dropdown">
 				<a href="contact.html">Shoes</a>
 			</li>
             <li class="dropdown">
 				<a href="contact.html">Kids & Girls</a>
 			</li>
-			
+
 			<li class="dropdown">
 				<a href="#" class="dropdown-toggle" data-hover="dropdown" data-toggle="dropdown">Pages</a>
 				<ul class="dropdown-menu pages">
 					<li>
 						<div class="yamm-content">
 							<div class="row">
-								
+
 									<div class="col-xs-12 col-menu">
 	                                  <ul class="links">
 		                                  	<li><a href="home.html">Home</a></li>
@@ -419,27 +419,27 @@
 											<li><a href="product-comparison.html">Product-Comparison</a></li>
 		                                  	<li><a href="faq.html">FAQ</a></li>
 											<li><a href="404.html">404</a></li>
-											
+
 	                                  </ul>
 									</div>
-									
-									
-								
+
+
+
 							</div>
 						</div>
 					</li>
-                    
-                   
-					
+
+
+
 				</ul>
 			</li>
              <li class="dropdown  navbar-right special-menu">
 				<a href="#">Todays offer</a>
 			</li>
-					
-			
+
+
 		</ul><!-- /.navbar-nav -->
-		<div class="clearfix"></div>				
+		<div class="clearfix"></div>
 	</div><!-- /.nav-outer -->
 </div><!-- /.navbar-collapse -->
 
@@ -517,7 +517,7 @@
                     <span><a href="#">flipmart@themesground.com</a></span>
                 </div>
             </li>
-              
+
             </ul>
     </div><!-- /.module-body -->
                 </div><!-- /.col -->
@@ -589,11 +589,11 @@
             <div class="col-xs-12 col-sm-6 no-padding">
                 <div class="clearfix payment-methods">
                     <ul>
-                        <li><img src="assets/images/payments/1.png" alt=""></li>
-                        <li><img src="assets/images/payments/2.png" alt=""></li>
-                        <li><img src="assets/images/payments/3.png" alt=""></li>
-                        <li><img src="assets/images/payments/4.png" alt=""></li>
-                        <li><img src="assets/images/payments/5.png" alt=""></li>
+                        <><img src="{{asset('frontend/assets/images/payments/1.png" alt=""></')}}li>
+                        <><img src="{{asset('frontend/assets/images/payments/2.png" alt=""></')}}li>
+                        <><img src="{{asset('frontend/assets/images/payments/3.png" alt=""></')}}li>
+                        <><img src="{{asset('frontend/assets/images/payments/4.png" alt=""></')}}li>
+                        <><img src="{{asset('frontend/assets/images/payments/5.png" alt=""></')}}li>
                     </ul>
                 </div><!-- /.payment-methods -->
             </div>
@@ -604,33 +604,33 @@
 
 
 	<!-- For demo purposes – can be removed on production -->
-	
-	
+
+
 	<!-- For demo purposes – can be removed on production : End -->
 
 	<!-- JavaScripts placed at the end of the document so the pages load faster -->
-	<script src="assets/js/jquery-1.11.1.min.js"></script>
-	
-	<script src="assets/js/bootstrap.min.js"></script>
-	
-	<script src="assets/js/bootstrap-hover-dropdown.min.js"></script>
-	<script src="assets/js/owl.carousel.min.js"></script>
-	
-	<script src="assets/js/echo.min.js"></script>
-	<script src="assets/js/jquery.easing-1.3.min.js"></script>
-	<script src="assets/js/bootstrap-slider.min.js"></script>
-    <script src="assets/js/jquery.rateit.min.js"></script>
-    <script type="text/javascript" src="assets/js/lightbox.min.js"></script>
-    <script src="assets/js/bootstrap-select.min.js"></script>
-    <script src="assets/js/wow.min.js"></script>
-	<script src="assets/js/scripts.js"></script>
+	< src="{{asset('frontend/assets/js/jquery-1.11.1.min.js"></')}}script>
+
+	< src="{{asset('frontend/assets/js/bootstrap.min.js"></')}}script>
+
+	< src="{{asset('frontend/assets/js/bootstrap-hover-dropdown.min.js"></')}}script>
+	< src="{{asset('frontend/assets/js/owl.carousel.min.js"></')}}script>
+
+	< src="{{asset('frontend/assets/js/echo.min.js"></')}}script>
+	< src="{{asset('frontend/assets/js/jquery.easing-1.3.min.js"></')}}script>
+	< src="{{asset('frontend/assets/js/bootstrap-slider.min.js"></')}}script>
+    < src="{{asset('frontend/assets/js/jquery.rateit.min.js"></')}}script>
+    < type="text/javascript" src="{{asset('frontend/assets/js/lightbox.min.js"></')}}script>
+    < src="{{asset('frontend/assets/js/bootstrap-select.min.js"></')}}script>
+    < src="{{asset('frontend/assets/js/wow.min.js"></')}}script>
+	< src="{{asset('frontend/assets/js/scripts.js"></')}}script>
 
 	<!-- For demo purposes – can be removed on production -->
-	
+
 	<script src="switchstylesheet/switchstylesheet.js"></script>
-	
+
 	<script>
-		$(document).ready(function(){ 
+		$(document).ready(function(){
 			$(".changecolor").switchstylesheet( { seperator:"color"} );
 			$('.show-theme-options').click(function(){
 				$(this).parent().toggleClass('open');
@@ -644,7 +644,7 @@
 	</script>
 	<!-- For demo purposes – can be removed on production : End -->
 
-	
+
 
 </body>
 </html>
